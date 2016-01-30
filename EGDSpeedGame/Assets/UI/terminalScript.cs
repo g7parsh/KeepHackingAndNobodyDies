7501586a2c191edfb1727d76bb10eae328ee5ee5
Craig Carlson <cjcarlson20@gmail.com>
2016-01-29 21:30:44 -0500

    made basic UI console, changed project settings for source control

---
+++ EGDSpeedGame/Assets/UI/terminalScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class terminalScript : MonoBehaviour {
+
+	public int numlines = 10;
+	public Text txt;
+	public InputField inputfield;
+
+	private string[] inputs;
+	private int count = 0;
+
+	void Awake() {
+		inputs = new string[numlines];
+		txt.text = "";
+	}
+
+	public void addLine(string line) {
+		string tmp = "";
+		
+
+		if (count < numlines) {//add to bottom
+			inputs[count] = line;
+			count++;
+		}
+		else {//shift everything then add to bottom
+			
+			for (int i = 0; i < numlines-1; i++) {
+				inputs[i] = inputs[i + 1];
+			}
+			inputs[numlines - 1] = line;
+
+
+		}
+
+		for (int i = 0; i < numlines; i++) {
+			tmp += inputs[i] + "\n";
+		}
+		
+
+
+		txt.text = tmp;
+
+		inputfield.text = "";
+		//reactivate input to type again
+		inputfield.ActivateInputField();
+	}
+}
