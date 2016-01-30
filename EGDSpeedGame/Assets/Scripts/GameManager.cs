a21d632efcf410b2a951485802b17e994aab8df9
Robert Rouhani <robert.rouhani@gmail.com>
2016-01-30 17:24:32 -0500

    Restored scripts lost in rebase #basedgit

---
+++ EGDSpeedGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	private StatusManager status;
+	public int Level = 1;
+	public int TotalLevels = 10;
+
+	public float MinigameWaitMinimum = 30;
+	public float MinigameWaitMaximum = 120;
+
+	// Use this for initialization
+	void Start () {
+		status = GetComponent<StatusManager>();
+
+		StartCoroutine(MinigameCoroutine());
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Level >= TotalLevels)
+		{
+			//TODO end game
+		}
+	}
+
+	public void CompleteMinigame(bool won)
+	{
+		if (!won)
+			status.Damage();
+		else
+			status.CompleteMinigame();
+	}
+
+	private IEnumerator MinigameCoroutine()
+	{
+		while (Level < TotalLevels)
+		{
+			yield return new WaitForSeconds((Random.value * (MinigameWaitMaximum - MinigameWaitMinimum)) + MinigameWaitMinimum);
+
+			LaunchMinigame();
+			
+			//TODO wait for minigame to be completed.
+		}
+	}
+
+	private void LaunchMinigame()
+	{
+		//TODO randomly select and display a minigame
+	}
+}
---
+++ EGDSpeedGame/Assets/Scripts/HealthNode.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthNode : MonoBehaviour {
+
+	public int Health = 2;
+
+	public bool IsDead { get { return Health <= 0; } }
+
+	public void Damage()
+	{
+		if (Health > 0)
+			Health--;
+	}
+}
