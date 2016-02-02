f350075d58b1d7c8ff25510d4d52dfd9e4655744
unknown <shahp2@SA2502-PC03.win.rpi.edu>
2016-02-02 15:47:02 -0500

    (Parsh)  Added simple script for controlling music. Math comes later

---
+++ EGDSpeedGame/Assets/MusicManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+    public AudioClip clip;
+    public AudioSource source;
+    public hackInterceptScript script;
+
+    void Awake() {
+        source.Play();
+    }
+	// Use this for initialization
+	
+    void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        source.pitch = script.speed;
+	}
+}
